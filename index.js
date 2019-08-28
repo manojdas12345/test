ce6bf5aa134a53e12524393181f85f3c2cadcf4d
manojdas12345 <54347817+manojdas12345@users.noreply.github.com>
2019-08-28 21:26:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-import "normalize.css/normalize.css";
-import React from "react";
-import ReactDOM from "react-dom";
+import AddressBook from "./AddressBook";
+import reducer from "./reducer";
 
-import { FakeHttpApi } from "./httpApi";
-import { setupStore } from "./store";
-import App from "./App";
-
-const reduxStore = setupStore({
-  httpApi: new FakeHttpApi(),
-});
-
-ReactDOM.render(
-  <App reduxStore={reduxStore} />,
-  document.getElementById("root"),
-);
+export default AddressBook;
+export { reducer };
--- reducer.js
+++ reducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from "redux";
 
-import { reducer as addressBookReducer } from "./AddressBook";
+import { reducer as searchReducer } from "./SearchContacts";
+import { reducer as contactDetailsReducer } from "./ContactDetails";
 
+// TODO something is wrong here
 export default combineReducers({
-  addressBook: addressBookReducer,
+  search: searchReducer,
+  contactDetails: () => ({}),
 });
